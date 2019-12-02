71fd0a18c7c1d029201dfd671db7820834203bf9
Iamicella Vincenzo <IamicellaVi@valueteam.com>
2019-12-02 20:26:06 +0100

    config-project

---
+++ config-server/src/main/java/it/codingjam/configserver/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package it.codingjam.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+// Infine per abilitare il config manager su Spring Boot sarà sufficiente inserire 
+// l’annotation @EnableConfigServer
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
