6f2f1a44faf376dc2e4a6a7d42fce7c657f5bcc9
Iamicella Vincenzo <IamicellaVi@valueteam.com>
2019-12-02 20:31:30 +0100

    zuul-gateway

---
+++ zuul-gateway/src/main/java/it/codingjam/zuulgateway/ZuulGatewayApplication.java
@@ -0,0 +1,25 @@
+package it.codingjam.zuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
+
+@ServletComponentScan
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class ZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+
+	@Bean
+	public AlwaysSampler defaultSampler() {
+		return new AlwaysSampler();
+	}
+}
